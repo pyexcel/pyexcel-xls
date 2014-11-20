f9c37ce3537607973e4aa957c6ac0583ef89142b
chfw <wangc_2011@hotmail.com>
2014-11-20 20:27:30 +0000

    fix setup typo

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ setup(
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
         'Intended Audience :: Developers',
         'Programming Language :: Python :: 2.6',
-        'Programming Language :: Python :: 2.7'
+        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: Implementation :: PyPy'
