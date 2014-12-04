651153c85a9fc2896ae45df95da71348fe58104b
chfw <wangc_2011@hotmail.com>
2014-12-04 22:43:43 +0000

    using pkgutil

--- pyexcel/ext/xl.py
+++ pyexcel/ext/xl.py
@@ -150,3 +150,21 @@ class XLWriter(BookWriter):
         This call actually save the file
         """
         self.wb.save(self.file)
+
+try:
+    from pyexcel.io import READERS
+    from pyexcel.io import WRITERS
+
+    READERS.update({
+        "xls": XLBook,
+        "xlsm": XLBook,
+        "xlsx": XLBook
+    })
+    WRITERS.update({
+        "xls": XLWriter
+    })
+except:
+    # to allow this module to function independently
+    pass
+
+__VERSION__ = "0.0.1"
