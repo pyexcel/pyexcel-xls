fdd10ed573370d85ddccf2d48780aad1dc67107d
chfw <wangc_2011@hotmail.com>
2016-07-12 17:49:52 +0100

    update version string and synchronize with latest pyexcel-commons

--- test.bat
+++ test.bat
@@ -1,2 +1,2 @@
 pip freeze
-nosetests --with-cov --cover-package pyexcel_xls --cover-package tests --with-doctest --doctest-extension=.rst tests README.rst pyexcel_xls  && flake8 . --exclude=.moban.d 
+nosetests --with-cov --cover-package pyexcel_xls --cover-package tests --with-doctest --doctest-extension=.rst tests README.rst pyexcel_xls  && flake8 . --exclude=.moban.d --builtins=unicode,xrange,long
