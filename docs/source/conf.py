7dad0f32610700ec35ab006a5643279de4d26701
chfw <wangc_2011@hotmail.com>
2017-12-08 22:44:57 +0000

    :handshake: synchronize with pyexcel-commons

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -10,11 +10,9 @@ extensions = [
     'sphinx.ext.intersphinx',
     'sphinx.ext.viewcode',
 ]
-
 intersphinx_mapping = {
     'pyexcel': ('http://pyexcel.readthedocs.io/en/latest/', None),
 }
-spelling_word_list_filename = 'spelling_wordlist.txt'
 templates_path = ['_templates']
 source_suffix = '.rst'
 master_doc = 'index'
@@ -22,7 +20,7 @@ master_doc = 'index'
 project = u'pyexcel-xls'
 copyright = u'2015-2017 Onni Software Ltd.'
 version = '0.5.4'
-release = '0.5.4'
+release = '0.5.5'
 exclude_patterns = []
 pygments_style = 'sphinx'
 html_theme = 'default'
