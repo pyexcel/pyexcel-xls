517f48c73189fe24f35db02bbd40d100e1dacc6b
Pyrox <pyrox@pyrox.dev>
2024-07-28 21:54:11 -0400

    update test scripts

--- test.sh
+++ test.sh
@@ -1,3 +1,3 @@
 #/bin/bash
 pip freeze
-nosetests --with-coverage --cover-package pyexcel_xls --cover-package tests tests --with-doctest --doctest-extension=.rst README.rst docs/source pyexcel_xls
+pytest
