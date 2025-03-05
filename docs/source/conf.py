05b0dc5a8ff446f037f3aff27419ef981d503359
chfw <wangc_2011@hotmail.com>
2025-03-05 16:25:47 +0000

    update test tool to pytest

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,7 +23,7 @@ DESCRIPTION = (
 # -- Project information -----------------------------------------------------
 
 project = 'pyexcel-xls'
-copyright = '2015-2022 Onni Software Ltd.'
+copyright = '2015-2025 Onni Software Ltd.'
 author = 'C.W.'
 # The short X.Y version
 version = '0.7.1'
